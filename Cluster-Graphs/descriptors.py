3f2bb905d5c5dac727e42db7a61400797b41f101
Yifan Wang <wangyf@udel.edu>
2018-07-20 11:59:32 -0400

    changes

--- Cluster-Graphs/descriptors.py
+++ Cluster-Graphs/descriptors.py
@@ -7,6 +7,7 @@ Created on Thu Jul 19 17:33:26 2018
 
 import sys
 import os
+import networkx as nx
 
 # working on all platforms, using cross-platform home path
 
@@ -15,12 +16,21 @@ sys.path.append(os.path.join(HomePath,'Documents','GitHub', 'Pdn-Dynamics-Model'
 
 import Cluster_structure as cs
 
-Descriptors = ['1',
-               '1-2',
-               '1-2 2-3',
-               '1-2 1-3 2-3',
-               '1-2 1-3 2-3 4-2 4-3',
-               '1-2 1-3 1-4',
-               '1-2 2-3 1-3 1-4']
+dev = ['1-2',
+       '1-2 2-3',
+       '1-2 1-3 2-3',
+       '1-2 1-3 2-3 4-2 4-3',
+       '1-2 1-3 1-4',
+       '1-2 2-3 1-3 1-4']
 
+edgev = []
 
+for i in range(len(dev)):
+    edgev.append(cs.neighbor_string_list([dev[i]]))
+cs.plot_graph(edgev)
+
+Pd1 = nx.Graph()
+Pd1.add_node(1) 
+gv = [Pd1]
+for i in range(len(edgev)):
+    gv.append(nx.Graph(edgev[i]))
