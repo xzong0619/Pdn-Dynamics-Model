3b559df62bf38031a58db459687e2a485289294d
Yifan Wang <wangyf@udel.edu>
2018-07-26 11:02:05 -0400

    changes

--- Cluster-Graphs/untitled4.py
+++ Cluster-Graphs/untitled4.py
@@ -6,13 +6,23 @@ Created on Wed Jul 25 13:26:00 2018
 """
 
 import networkx as nx
-from networkx.algorithms import isomorphism as isomo
+import matplotlib.pyplot as plt
+from networkx.algorithms import isomorphism as iso
 
-G1 =  nc_v[2]
+G1 =  nc_v[5]
+print(G1.nodes(data=True))
 plt.figure()
 nx.draw(G1, with_labels=True)
-G2 = gv[1]
+G2 = gv[2]
+print(G2.nodes(data=True))
 plt.figure()
 nx.draw(G2, with_labels=True)
-GM = isomo.GraphMatcher(G1,G2)
-x = GM.isomorphisms_iter()
+GM = iso.GraphMatcher(G1,G2)
+x= []
+for mapping in GM.subgraph_isomorphisms_iter():
+        print (mapping)
+        x.append(mapping)
+#x = [y for y in ]
+#z = GM.mapping()
+#x = [y for y in GM.isomorphisms_iter()]
+#print(z)
