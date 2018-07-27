2c1406e5784a6b0b203e77b5a40025a130c3f4d7
Yifan Wang <wangyf@udel.edu>
2018-07-27 17:06:01 -0400

    changes

--- Cluster-Graphs/untitled4.py
+++ Cluster-Graphs/untitled4.py
@@ -8,16 +8,17 @@ Created on Wed Jul 25 13:26:00 2018
 import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.algorithms import isomorphism as iso
+from pprint import pprint
 
 G1 =  Gsv[7]
-print(G1.nodes(data=True))
 plt.figure()
 drawing(G1)
 G2 = Gcv[4]
-print(G2.nodes(data=True))
 plt.figure()
 drawing(G2)
 
-GM = iso.GraphMatcher(G1, G2, edge_match=iso.categorical_edge_match(['length'],[]))
+GM = iso.GraphMatcher(G1, G2, edge_match=iso.numerical_edge_match(['length'],[2.0]))
 x= [y for y in GM.subgraph_isomorphisms_iter()]
 
+print(len(x))
+pprint(x)
