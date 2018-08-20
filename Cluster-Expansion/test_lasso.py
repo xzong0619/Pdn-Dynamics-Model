b9a49dc46931749ce6b46809857e2b90bf20680c
Yifan Wang <wangyf@udel.edu>
2018-08-19 22:42:07 -0400

    change lasso

--- Cluster-Expansion/test_lasso.py
+++ Cluster-Expansion/test_lasso.py
@@ -13,19 +13,32 @@ import sklearn.cross_validation as cv
 import numpy as np
 from structure_constants import Ec
 
-X = np.load('pi3.npy')
+x = np.load('pi3.npy')
+X = np.ones((x.shape[0], x.shape[1]+1))
+X[:,1:] = x        
+    
+#%%
+
 y = np.array(Ec)
 
 
 X_train, X_test, y_train, y_test = cv.train_test_split(X, y, test_size=0.1, random_state=0)
 
+rkf = RepeatedKFold(n_splits = 10, n_repeats = 5)
 
-lasso_cv  = LassoCV()
-
-rkf = RepeatedKFold(n_splits = 5, n_repeats = 1)
-
+lasso_cv  = LassoCV(cv = rkf)
 lasso_cv.fit(X_train, y_train)
-alphas, coef_path, _ = lasso_path(X_train, y_train, fit_intercept=False)
-alpha = lasso_cv.alpha_
 
+#alphas, coef_path, _ = lasso_path(X_train, y_train, fit_intercept=False)
+alpha = lasso_cv.alpha_
+alphas = lasso_cv.alphas_
+coefs = lasso_cv.coef_
 y_predict = lasso_cv.predict(X_test)
+
+MSE_predict = sum((y_test - y_predict))**2/len(y_test)
+MSE_train = sum((y_train - lasso_cv.predict(X_train)))**2/len(y_train)
+
+#%%
+J_index = np.nonzero(coefs)[0] -1
+J_nonzero = coefs[np.nonzero(coefs)]
+
