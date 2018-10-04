fe84cafccd9cebe244454ad237f8bff706a05c31
Yifan Wang <wangyf@udel.edu>
2018-10-04 00:55:08 -0400

    changes

--- Cluster-Expansion/v2/test_connectivity_fitness.py
+++ Cluster-Expansion/v2/test_connectivity_fitness.py
@@ -16,11 +16,11 @@ def initialize_Clusters_object():
     '''
     only draw 1st nearest neighbors?
     '''
-    NN1 = 0
+    NN1 = 1
     '''
     Draw mother/conifgurations/clusters?
     '''
-    draw = [0, 0, 0]
+    draw = [0, 1, 0]
     
     
     Clusters = lf.clusters(occ, NN1, draw)
@@ -35,21 +35,37 @@ def get_occupied_nodes(colorsv):
     nodes = len(colorsv)
     boov =  [color == 'r' for color in colorsv]
     occ_nodes = np.arange(nodes)[np.array(boov)]
-    
- 
+
     return occ_nodes
     
+def get_possible_edges(nodes):
+    
+    edges = []
+    nn = len(nodes) 
+    for i in range(nn):
+            for j in np.arange(i+1,nn):
+                edges.append((nodes[i],nodes[j]))
+    return edges
+    
+def get_edge_score(edges, Gsv):
+    
+    count = 0
+    for edge in edges:
+        if edge in Gsv.edges: count = count+1
+    score = count/len(edges)
+    
+    return score
 
-def get_neighbor_colors(node, Gsv):
-    
-    neighbors = list(nx.all_neighbors(Gsv, i))
-    edges = 
-    
 Clusters = initialize_Clusters_object()
 occ = Clusters.occupancy
-
-ind = [0,1,1,1,0,0,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0]
+'''
+ind = [1,1,1,1]
 ind_Gsv = GA.individual_config(ind, Clusters)[0]
-
+'''
+ind_list = [24,21,1,2,3]
+Clusters.get_configs([ind_list])
+ind_Gsv = Clusters.Gsv[0]
 node_color = get_color(ind_Gsv)
 occ_nodes = get_occupied_nodes(node_color)
+occ_edges = get_possible_edges(occ_nodes)
+score = get_edge_score(occ_edges, ind_Gsv)
