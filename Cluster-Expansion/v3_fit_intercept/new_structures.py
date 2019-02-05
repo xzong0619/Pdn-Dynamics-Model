4d8de753b63f7f827f76b8f2b5d9645b3e362770
Yifan Wang <wangyf@udel.edu>
2019-02-04 20:19:06 -0500

    changes

--- Cluster-Expansion/v3_fit_intercept/new_structures.py
+++ Cluster-Expansion/v3_fit_intercept/new_structures.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 from structure_constants import NPd_list, Ec
-import GA_functions as GA
-from ase.io import read, write
-from ase.visualize import view
+#import GA_functions as GA
+#from ase.io import read, write
+#from ase.visualize import view
 
 font = {'size'   : 20}
 
@@ -119,30 +119,30 @@ plt.yticks(np.arange(-25, 5, 5))
 plt.legend()
 plt.show()
 
-
-# Create atom objects for the qualified individuals 
-ns = 2 # frome each group select 2 
-new_structure_pool = []
-new_atoms = []
-for Pdi in nPd_v:
-    for si in range(ns):
-        new_structure_pool.append(new_Pd_min[Pdi][si])
-        new_atoms.append(GA.ase_object(new_Pd_min[Pdi][si]))
-
-
-pov_args = {
-	'transparent': True, #Makes background transparent. I don't think I've had luck with this option though
-    'canvas_width': 3600., #Size of the width. Height will automatically be calculated. This value greatly impacts POV-Ray processing times
-    'display': False, #Whether you want to see the image rendering while POV-Ray is running. I've found it annoying
-    'rotation': '0x, 0y,0z', #Position of camera. If you want different angles, the format is 'ax, by, cz' where a, b, and c are angles in degrees
-    'celllinewidth': 0.02, #Thickness of cell lines
-    'show_unit_cell': 0 #Whether to show unit cell. 1 and 2 enable it (don't quite remember the difference)
-    #You can also color atoms by using the color argument. It should be specified by an list of length N_atoms of tuples of length 3 (for R, B, G)
-    #e.g. To color H atoms white and O atoms red in H2O, it'll be:
-    #colors: [(0, 0, 0), (0, 0, 0), (1, 0, 0)]
-    }
-#%%
-#Write to POV-Ray file
-for k, atomi in enumerate(new_atoms):
-    write('structure' + str(k)+'.POV', atomi, **pov_args)
+#
+## Create atom objects for the qualified individuals 
+#ns = 2 # frome each group select 2 
+#new_structure_pool = []
+#new_atoms = []
+#for Pdi in nPd_v:
+#    for si in range(ns):
+#        new_structure_pool.append(new_Pd_min[Pdi][si])
+#        new_atoms.append(GA.ase_object(new_Pd_min[Pdi][si]))
+#
+#
+#pov_args = {
+#	'transparent': True, #Makes background transparent. I don't think I've had luck with this option though
+#    'canvas_width': 3600., #Size of the width. Height will automatically be calculated. This value greatly impacts POV-Ray processing times
+#    'display': False, #Whether you want to see the image rendering while POV-Ray is running. I've found it annoying
+#    'rotation': '0x, 0y,0z', #Position of camera. If you want different angles, the format is 'ax, by, cz' where a, b, and c are angles in degrees
+#    'celllinewidth': 0.02, #Thickness of cell lines
+#    'show_unit_cell': 0 #Whether to show unit cell. 1 and 2 enable it (don't quite remember the difference)
+#    #You can also color atoms by using the color argument. It should be specified by an list of length N_atoms of tuples of length 3 (for R, B, G)
+#    #e.g. To color H atoms white and O atoms red in H2O, it'll be:
+#    #colors: [(0, 0, 0), (0, 0, 0), (1, 0, 0)]
+#    }
+##%%
+##Write to POV-Ray file
+#for k, atomi in enumerate(new_atoms):
+#    write('structure' + str(k)+'.POV', atomi, **pov_args)
 
