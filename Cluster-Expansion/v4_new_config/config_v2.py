377f1c3fec029a5914ce27160cd745f36f53ef21
Yifan Wang <wangyf@udel.edu>
2019-02-15 11:53:55 -0500

    changes

--- Cluster-Expansion/v4_new_config/config_v2.py
+++ Cluster-Expansion/v4_new_config/config_v2.py
@@ -9,13 +9,16 @@ import lattice_functions as lf
 import numpy as np
 import pandas as pd
 import pickle
+from itertools import combinations, product
+
+
 
 node_layer_v = np.around(mother[:,2]/dz, decimals = 0).astype(int)
 node_layer = dict()
 n_layer = np.amax(node_layer_v)
 
 for layer_i in range(n_layer):
-    node_layer[layer_i +1] = np.where(node_layer_v == layer_i +1)[0] + 1   
+    node_layer[layer_i +1] = list(np.where(node_layer_v == layer_i +1)[0] + 1)   
     
 config_i = config[6]
 config_layers = np.around(mother[np.array(config_i)][:,2]/dz, decimals = 0).astype(int)
@@ -24,9 +27,24 @@ layer_count = []
 for layer_i in range(n_layer):  
     layer_count.append(len(np.where(config_layers == layer_i + 1)[0]))
 
-    
-    
-    
+layer_count = list(np.array(layer_count)[np.nonzero(layer_count)[0]])
+
+combo_dict = dict()
+
+for i,layer_count_i in enumerate(layer_count):
+    layer_i  = i + 1
+    combo_dict[layer_i] =  list(combinations(node_layer[layer_i], layer_count_i))
+
+combo = combo_dict[1]
+for i in range(1,len(layer_count)):
+    combo = product(combo, combo_dict[i+1])
+combo = list(combo)   
+for i,combo_i in enumerate(combo):
+    temp = ()
+    for j, combo_ij in enumerate(combo_i):
+        temp = temp + combo_i[j]
+    combo[i] = temp
+#    
 #empty = 'grey'
 #filled = 'r'
 #occ = [empty, filled]
