bb60f98e9a27cbb1674e0d4792932ddb6fda627c
Yifan Wang <wangyf@udel.edu>
2019-02-20 17:11:13 -0500

    change test mpi

--- Cluster-Expansion/v5_lasso/test_mpi.py
+++ Cluster-Expansion/v5_lasso/test_mpi.py
@@ -2,9 +2,9 @@ from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
-print(size)
 rank = comm.Get_rank()
-print(rank)
+print("%d of %d" % (comm.Get_rank(), comm.Get_size()))
+
 if rank == 0:
    data = [(x+1)**x for x in range(size)]
    print('we will be scattering:{}'.format(data))
@@ -15,7 +15,10 @@ data = comm.scatter(data, root=0)
 data += 1
 print('rank {}  has data: {}.'.format(rank, data))
 
+print('Core {} Before gather.'.format(rank))
+
 newData = comm.gather(data,root=0)
 
+print('Core {} After gather.'.format(rank))
 if rank == 0:
    print('master:{}'.format(newData))
