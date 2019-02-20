2de9d3fe4c4752b8b22c491d20b65ab15085c446
Yifan Wang <wangyf@udel.edu>
2019-02-20 17:20:14 -0500

    changes

--- Cluster-Expansion/v5_lasso/test_mpi.py
+++ Cluster-Expansion/v5_lasso/test_mpi.py
@@ -23,6 +23,10 @@ print('Core {} After gather.'.format(rank))
 if rank == 0:
    print('master:{}'.format(newData))
 
+'''
+Commend:
+mpirun -n 4 python test_MPI.py
+'''
 
 '''
 Caviness output
@@ -51,4 +55,23 @@ Core 3 After gather.
 '''
 '''
 Mac output
+
+0 of 4
+we will be scattering:[1, 2, 9, 64]
+1 of 4
+rank 0  has data: 2.
+Core 0 Before gather.
+2 of 4
+rank 2  has data: 10.
+Core 2 Before gather.
+Core 2 After gather.
+rank 1  has data: 3.
+Core 1 Before gather.
+Core 1 After gather.
+3 of 4
+rank 3  has data: 65.
+Core 3 Before gather.
+Core 3 After gather.
+Core 0 After gather.
+master:[2, 3, 10, 65]
 '''
